09a7d3a43e46f87fbbbe5488a38dbd4a780f284f
Rafael Santos <rafael.santos@acal.local>
2022-11-19 11:20:35 -0300

    add more text in page about

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -5,14 +5,14 @@ export const About = () => {
     <div style={{
       width: '80vw',
       height: '100vh',
-      backgroundColor: 'yellow',
+      backgroundColor: 'blue',
       border: '1px solid black',
 
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center'
     }}>
-      <h1 style={{color: "red"}}>About</h1>
+      <h1 style={{color: "white"}}>About page in development</h1>
     </div>
   )
 }
