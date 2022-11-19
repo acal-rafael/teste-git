5f9c5f60a1352a4ff2fcc1d27d72282f93c880c6
Rafael Santos <rafaelsvbr@gmail.com>
2022-11-19 11:34:57 -0300

    add div-children in page-home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,13 +13,25 @@ export const Home = () => {
       justifyContent: 'center',
       alignItems: 'center',
     }}>
-      <h1 style={{
-        width: '200px',
-        height: '100px',
-        color: "blue",
-
+      <div style={{
+        width: '80%',
+        height: '50%',
+        margin: '0 auto',
         border: '1px solid black',
-      }}>Home</h1>
+        backgroundColor: 'brown',
+
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <h1 style={{
+          width: '200px',
+          height: '100px',
+          color: "white",
+
+          border: '1px solid black',
+        }}>Home</h1>
+      </div>
     </div>
   )
 }
