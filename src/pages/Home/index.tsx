066188569cfa1ccba5a62a1d1e237195c53285d6
Rafael Santos <rafaelsvbr@gmail.com>
2022-11-19 09:12:24 -0300

    add div father in page home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,23 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <h1 style={{color: "green"}}>Home</h1>
+    <div style={{
+      width: '80vw',
+      height: '100vh',
+      margin: '0 auto',
+      border: '1px solid black',
+
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <h1 style={{
+        width: '200px',
+        height: '100px',
+        color: "green",
+
+        border: '1px solid black',
+      }}>Home</h1>
+    </div>
   )
 }
